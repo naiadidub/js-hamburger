e391efd227123be0f1253e6a78f6d67ec83763ae
naiadidub <antonio.zaghi1405@gmail.com>
2022-03-28 09:53:31 +0200

    added close

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let pippo = document.querySelector('.header-right > a')
-console.log(pippo)
+let mars = document.querySelector('.close')
 
 pippo.addEventListener("click", pluto);
+mars.addEventListener("click", june)
 
 function pluto() {
     document.querySelector('.hamburger-menu').classList.add('active')
+}
+function june() {
+    document.querySelector('.hamburger-menu').classList.remove('active')
 }
 
